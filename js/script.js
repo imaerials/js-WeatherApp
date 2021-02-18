b6ea7425b773e49b4f645711251dc4d811ca15b8
imaerials <arielro85@gmail.com>
2021-02-17 22:50:48 -0300

    Weather 5 days forecast in the console

--- js/script.js
+++ js/script.js
@@ -8,11 +8,35 @@ let image = document.querySelector('.weather__image')
 let temperature = document.querySelector('.weather__temperature>.value')
 let searchInput = document.querySelector('.weather__search')
 let apiKey = "27bd91e7c5a2859fd0e65317b9fa86ba"
-let weatherBaseQuery = "http://api.openweathermap.org/data/2.5/weather?appid=" + apiKey
+let weatherBaseQuery = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + apiKey
+let forecastBaseQuery = "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=" + apiKey
+
+let cityForecastbyId = async (id) => {
+    let endpoint = forecastBaseQuery + '&id=' + id
+    let response = await fetch(endpoint)
+    let forecast = await response.json();
+    let forecastList = forecast.list;
+    let daily = [];
+
+    forecastList.forEach(day => {
+        let date = new Date(day.dt_txt.replace(' ','T'));
+        let hours= date.getHours()
+        if (hours === 12) {
+            daily.push(day)
+            
+        }
+         
+    })
+    console.log(daily)
+
+    //console.log(forecastList)
+}
 
 let getCityWeather = async (city) => {
-    let endpoint = weatherBaseQuery + '&q=' + city + '&units=metric';
+    let endpoint = weatherBaseQuery + '&q=' + city;
     let response = await fetch(endpoint);
+    //console.log(response)
+
     let weather = await response.json();
     return weather;
 
@@ -20,17 +44,19 @@ let getCityWeather = async (city) => {
 searchInput.addEventListener('keydown', async (e) => {
     if (e.keyCode === 13) {
         let weather = await getCityWeather(searchInput.value)
-        console.log(weather)
+        let cityId = weather.id
+        //console.log(cityId)
+        cityForecastbyId(cityId)
         updateCurrentWeather(weather)
 
     }
 
 })
 
-let updateCurrentWeather = (currentWeatherInfo)=>{
+let updateCurrentWeather = (currentWeatherInfo) => {
     city.textContent = currentWeatherInfo.name
     humidity.textContent = currentWeatherInfo.main.humidity
-    temperature.textContent = currentWeatherInfo.main.temp
+    temperature.textContent = Math.round(currentWeatherInfo.main.temp)
 
 
 }
